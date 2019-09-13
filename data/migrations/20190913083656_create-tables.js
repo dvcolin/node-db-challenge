21905ebed125f4960acbfa9a81264f1eccbd2d23
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-13 08:54:14 -0700

    added schema for resources and tasks tables

--- data/migrations/20190913083656_create-tables.js
+++ data/migrations/20190913083656_create-tables.js
@@ -9,16 +9,44 @@ exports.up = function(knex) {
       tbl.string('name', 255).notNullable();
 
       // PROJECT DESCRIPTION
-      tbl.string('description', 1000);
+      tbl.text('description');
 
       // PROJECT COMPLETED BOOLEAN
-      tbl.boolean('completed').defaultTo(false);
+      tbl.boolean('completed').defaultTo(false).notNullable();
       
   })
   .createTable('resources', tbl => {
+      // ID
+      tbl.increments();
+
+      // RESOURCE NAME
+      tbl.string('name', 255).unique().notNullable();
+
+      // RESOURCE DESCRIPTION
+      tbl.text('description');
 
   })
   .createTable('tasks', tbl => {
+      // ID
+      tbl.increments();
+
+      // TASK DESCRIPTION
+      tbl.text('description').notNullable();
+
+      // TASK NOTES
+      tbl.text('notes');
+
+      // TASK COMPLETED BOOLEAN
+      tbl.boolean('completed').defaultTo(false).notNullable();
+
+      // FOREIGN KEY TO PROJECTS TABLE
+      tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('projects')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
 
   })
   .createTable('project-resources', tbl => {
